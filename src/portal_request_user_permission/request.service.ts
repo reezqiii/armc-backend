7e784b58b70007e9f908ec0856b86b96116355a7
Rizqi Vela Syifa <velasyifa02@gmail.com>
2026-02-09 10:18:31 +0700

    UP-ARMC-B-00000067
    - refactor(permissions): simplify filter & add id checks

--- src/portal_request_user_permission/request.service.ts
+++ src/portal_request_user_permission/request.service.ts
@@ -1250,11 +1250,9 @@ export class RequestService {
       32,
     );
 
-    const leadItPermissions = permissions
-      .filter((p) => p.index_key === "0")
-      .map((p) => p.id_portal_permission);
+    const leadItPermissions = permissions.filter((p) => p.index_key === "0");
 
-    if (!leadItPermissions.includes("2000")) {
+    if (!leadItPermissions.length) {
       throw new ForbiddenException("Not allowed to approve as Lead IT");
     }
 
@@ -1314,11 +1312,9 @@ export class RequestService {
       32,
     );
 
-    const leadItPermissions = permissions
-      .filter((p) => p.index_key === "0")
-      .map((p) => p.id_portal_permission);
+    const leadItPermissions = permissions.filter((p) => p.index_key === "0");
 
-    if (!leadItPermissions.includes("2000")) {
+    if (!leadItPermissions.length) {
       throw new ForbiddenException("Not allowed to approve as Lead IT");
     }
 
@@ -1384,11 +1380,9 @@ export class RequestService {
       32,
     );
 
-    const itManagerPermissions = permissions
-      .filter((p) => p.index_key === "1")
-      .map((p) => p.id_portal_permission);
+    const itManagerPermissions = permissions.filter((p) => p.index_key === "1");
 
-    if (!itManagerPermissions.includes("2001")) {
+    if (!itManagerPermissions.length) {
       throw new ForbiddenException("Not allowed to approve as IT Manager");
     }
 
